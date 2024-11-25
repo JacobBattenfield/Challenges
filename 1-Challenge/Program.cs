fbf574983db6a8d08294980e07a1ca6ac83e733d
jaybat-code <jacobbattenfielddev@gmail.com>
2024-11-25 07:40:16 -0600

    Box Model

--- 1-Challenge/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program{
-    static void Main(){
-        Console.WriteLine("              )");
-        Console.WriteLine("             (");
-        Console.WriteLine(" _ ___________ )");
-        Console.WriteLine("[_[___________#");
-    }
-}
